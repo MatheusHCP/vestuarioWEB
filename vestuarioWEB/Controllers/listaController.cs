48d7e570c8dc595de291caf7eeb82c764f19a16c
MATHEUS HENRIQUE CARVALHO PEREIRA <matheuspereira@toledoprudente.edu.br>
2021-10-20 20:54:38 -0300

    Criação da view Lista de categorias e marcas

---
+++ vestuarioWEB/Controllers/listaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vestuarioWEB.Controllers
+{
+    public class listaController : Controller
+    {
+        public IActionResult listaCategorias()
+        {
+            return View();
+        }
+        public IActionResult listaMarcas()
+        {
+            return View();
+        }
+    }
+}
