994d746d7411f880bfce33487a0dcc883cd5ef4f
Mat_P <Mat_P@DESKTOP-N7RLFKA>
2021-10-20 20:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ vestuarioWEB/Controllers/cadastrosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vestuarioWEB.Controllers
+{
+    public class cadastrosController : Controller
+    {
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+
+        public IActionResult Marcas()
+        {
+            return View();
+        }
+    }
+}
